5d5d1788c77caedc55f797b745472b32def6d194
Romainducos <ducosromain31@gmail.com>
2025-10-30 16:28:16 +0100

    Feature : added navbar

--- Portfolio/src/components/Accueil.jsx
+++ Portfolio/src/components/Accueil.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './index.css'
+import '../index.css'
+import Navbar from './Navbar.jsx'
 
 function Accueil() {
   return (
-    <h1>Hello </h1>
+    <Navbar />
   )
 }
 
--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Accueil from './Accueil.jsx'
+import Accueil from './components/Accueil.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
